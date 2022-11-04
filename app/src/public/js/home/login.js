1a965dc5d03dbcf7e3a8b99d1de6fffaf0b94fdf
bjmin17@naver.com <bjmin17@naver.com>
2022-11-04 13:42:50 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -16,4 +16,15 @@ function login() {
         psword: psword.value
     }
     console.log(req);
+    console.log(JSON.stringify(req));
+    
+    
+    
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(req),
+    })
 }
