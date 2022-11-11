cc4cb9ddcee66f3e15f20d45c0b67cc95c9b63fc
bjmin17@naver.com <bjmin17@naver.com>
2022-11-11 15:11:53 +0900

    백엔드 맛보기 최적화

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -10,10 +10,9 @@ class User {
     async login() {
         const client = this.body;
         try {
-            const { id, psword } = await UserStorage.getUserInfo(client.id);
-        
-            if(id) {
-                if( id === client.id && psword === client.psword) {
+            const user = await UserStorage.getUserInfo(client.id);
+            if(user) {
+                if( user.id === client.id && user.psword === client.psword) {
                     return {success : true};
                 }
                 return {success : false, msg: "비밀번호가 틀렸습니다."};
@@ -22,8 +21,6 @@ class User {
         } catch (err) {
             return { success: false, err};
         }
-        
-        
     }
 
     async register() {
